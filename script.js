c9a2671fa75855d0d75e62a642e742401a6f2401
Nabil Hadi <napilfadl@gmail.com>
2022-09-05 09:52:58 +0200

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello world!");
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * 3)];
+}
+
+getComputerChoice();
