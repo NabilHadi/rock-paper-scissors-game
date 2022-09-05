2b99839bd6508f952ab2cb2eaa0ace279740aa18
Nabil Hadi <napilfadl@gmail.com>
2022-09-05 10:08:16 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -4,4 +4,61 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-getComputerChoice();
+function playRound(playerChoice, computerChoice) {
+  playerChoice = playerChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  if (playerChoice === "rock") {
+    if (computerChoice === "rock") {
+      return "It's a draw!";
+    } else if (computerChoice === "paper") {
+      return "You lose! Paper beats Rock";
+    } else if (computerChoice === "scissors") {
+      return "You win! Rock beats Scissors";
+    } else {
+      return (
+        "Error computerChoice: " +
+        computerChoice +
+        ", playerChoice: " +
+        playerChoice
+      );
+    }
+  } else if (playerChoice === "paper") {
+    if (computerChoice === "rock") {
+      return "You win! Paper beats Rock";
+    } else if (computerChoice === "paper") {
+      return "It's a draw!";
+    } else if (computerChoice === "scissors") {
+      return "You lose! Scissors beats Paper";
+    } else {
+      return (
+        "Error computerChoice: " +
+        computerChoice +
+        ", playerChoice: " +
+        playerChoice
+      );
+    }
+  } else if (playerChoice === "scissors") {
+    if (computerChoice === "rock") {
+      return "You lose! Rock beats Scissors";
+    } else if (computerChoice === "paper") {
+      return "You win! Scissors beats Paper";
+    } else if (computerChoice === "scissors") {
+      return "It's a draw!";
+    } else {
+      return (
+        "Error computerChoice: " +
+        computerChoice +
+        ", playerChoice: " +
+        playerChoice
+      );
+    }
+  } else {
+    return (
+      "Error computerChoice: " +
+      computerChoice +
+      ", playerChoice: " +
+      playerChoice
+    );
+  }
+}
